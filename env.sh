b2538f5f8779f67dd78890017b5f509a578de17a
SyC Inversiones <admin@sycinversiones.com.ar>
2023-03-06 10:39:04 -0300

    Mejoras en defión de BPM_HOME

--- env.sh
+++ env.sh
@@ -3,3 +3,7 @@
 # REPLACE repos/my_repo_name WITH THE ACTUAL BPM REPO PATH
 
 BMP_PROJECT_DIR=repos/my_repo_name
+
+if [[ ! $BPM_HOME ]]; then
+  BPM_HOME=ubuntu@bpmtest.sycinversiones.com:/home/ubuntu/bpmtest/files/BpmApp/$BMP_PROJECT_DIR
+fi
