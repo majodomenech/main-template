d31c29fc08b8e4e183922e888a487ffdd601b20b
SyC Inversiones <admin@sycinversiones.com.ar>
2023-03-06 10:49:57 -0300

    Mejoras en defión de BPM_HOME

--- env.sh
+++ env.sh
@@ -4,6 +4,9 @@
 
 BMP_PROJECT_DIR=repos/my_repo_name
 
+
+# This will set BPM_HOME to default, if not defined.
+# To define BPM_HOME in your environment, add your own setting to .profile or .bash_profile.
 if [[ ! $BPM_HOME ]]; then
   BPM_HOME=ubuntu@bpmtest.sycinversiones.com:/home/ubuntu/bpmtest/files/BpmApp
 fi
