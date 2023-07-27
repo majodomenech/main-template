ad96f25b1e1811964359790f6c19017571c4be1e
Administador <admin@sycinversiones.com>
2023-07-27 17:08:34 -0300

    generar api

--- gen-code.sh
+++ gen-code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker run -v $(pwd):/local swaggerapi/swagger-codegen-cli-v3 generate -l python -c /local/config.json -o /local/tmp -i /local/api.yaml
+sudo docker run -v $(pwd):/local swaggerapi/swagger-codegen-cli-v3 generate -l python -c /local/config.json -o /local/tmp -i /local/openapi/api.yaml
 sudo cp -rf tmp/blapi src
 sudo cp -rf tmp/docs/* doc
 sudo rm -Rf tmp
