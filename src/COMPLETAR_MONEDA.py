20f36122c461932606a5fdaa7b885adcd8559fa3
electric-lady <lioren.bessone.jofre@mi.unc.edu.ar>
2023-12-04 16:50:41 -0300

    corrigiendo el form que autocompleta moneda

--- src/COMPLETAR_MONEDA.py
+++ src/COMPLETAR_MONEDA.py
@@ -1,6 +1,7 @@
 #!python3
 import psycopg2
-import psucopg2.extras
+import psycopg2.extras
+import re
 import redflagbpm
 def _get_hg_connection(DB):
     #Manual connection, no config file
@@ -12,31 +13,25 @@ def _get_hg_connection(DB):
     conn.autocommit = True
     return conn
 
-def get_moneda_fondo(fondo):
+def get_moneda_fondo(codigo_fci):
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
     sql = """
         select mon."CODIGO"
         from "UNI_UNIDAD" fci
             inner join "UNI_UNIDAD" mon on mon."UNI_UNIDAD_ID" = fci."MONEDA"
         where fci."CODIGO"=%s
         """
-
-    cur.execute(sql, (fondo, ))
-    moneda = cur.fetchone()
+    cur.execute(sql, (codigo_fci, ))
+    qry = cur.fetchone()
+    moneda= qry['CODIGO']
     cur.close()
     conn.close()
     return moneda
-
-
 
 if __name__ == '__main__':
     bpm = redflagbpm.BPMService()
     fondo = bpm.context.input['fondo']
     conn = _get_hg_connection('syc')
     moneda = get_moneda_fondo(fondo)
-
     #setear moneda en el form
     bpm.context.input["moneda"] = moneda
-
-
