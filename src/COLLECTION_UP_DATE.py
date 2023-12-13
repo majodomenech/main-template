b1134eff934fd2ee9503f4844c47964a39eb1469
electric-lady <lioren.bessone.jofre@mi.unc.edu.ar>
2023-12-13 13:59:57 -0300

    incluyendo un script que calcula el timestamp now y lo setea en la coleccion cuando modifican el precio

---
+++ src/COLLECTION_UP_DATE.py
@@ -0,0 +1,12 @@
+# !python3
+from datetime import datetime
+import time
+import redflagbpm
+bpm = redflagbpm.BPMService()
+
+current_datetime = datetime.now()
+# Convert the datetime object to a timestamp in seconds
+timestamp_seconds = time.mktime(current_datetime.timetuple())
+# Convert the timestamp to milliseconds
+timestamp_milliseconds = timestamp_seconds * 1000
+bpm.context.input['fecha_cotizacion'] = timestamp_milliseconds
