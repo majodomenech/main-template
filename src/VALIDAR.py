db4846439f10b2c5eeb6d0b556d1512eb9ceb236
electric-lady <lioren.bessone.jofre@mi.unc.edu.ar>
2023-12-22 11:54:02 -0300

    modificando falidaciones de forms iniciales

--- src/VALIDAR.py
+++ src/VALIDAR.py
@@ -1,47 +1,31 @@
 #!python3
 import redflagbpm
-import re
 
-bpm = redflagbpm.BPMService()
-error = '[[[No hay solicitudes pendientes]]]'
-try:
-    array_solicitud_pendiente = bpm.context.input['array_solicitud_pendiente']
+def validar_solicitudes_no_vacias(bpm):
+    error = '[[[No hay solicitudes pendientes]]]'
+    try:
+        array_solicitud_pendiente = bpm.context.input['array_solicitud_pendiente']
+        try:
+            array_solicitud_confirmada = bpm.context.input['array_solicitud_confirmada']
+        except:
+            array_solicitud_confirmada = None
 
-    # BusinessKey = bpm.execution.getBusinessKey()
-    # tipo_solicitud = re.match(r'([a-zA-Z]+)', BusinessKey).group(1)
-    # # validación para rescates: si usan o no calcular tiene que validar la cadena de clicks adecuada
-    # validar_solicitar_string = ""
-    # if tipo_solicitud == 'INSTFCIRE':
-    #     for solicitud in array_solicitud_pendiente:
-    #         fondo_deno = solicitud['fondo']
-    #         cantidad_importe = solicitud['cantidad_importe']
-    #         monto = solicitud['monto']
-    #         validar_solicitar_string += f"{fondo_deno}_{cantidad_importe}_{monto}"
-    #     try:
-    #         calcular_solicitar_string = bpm.context['calcular_solicitar_string']
-    #         if validar_solicitar_string != calcular_solicitar_string:
-    #             bpm.fail(f"Revisa la carga {bpm.context['initiator']}")
-    #             raise Exception(u'[[[Revisa la carga mi ciela 👁👄👁 💅!]]]')
-    #     except:
-    #         pass
-    #
-    if array_solicitud_pendiente is None:
+        if (array_solicitud_pendiente is None or len(array_solicitud_pendiente) == 0) and (array_solicitud_confirmada is None or len(array_solicitud_confirmada) == 0):
+            bpm.fail(error)
+    except:
         bpm.fail(error)
-except:
-    bpm.fail(error)
 
-if len(array_solicitud_pendiente) == 0:
-    bpm.fail(error)
+def custom_message(tipoResultado):
+    if tipoResultado == "finalizar":
+        message = "Tarea cancelada"
+    else:
+        message = "Solicitud procesada"
 
-
-tipoResultado = bpm.context.input['tipoResultado']
-if tipoResultado == "finalizar":
-    message = "Tarea cancelada"
-else:
-    message = "Solicitud procesada"
-
-bpm.context.input['message'] = message
+    bpm.context.input['message'] = message
 
 
-
-
+if __name__ == '__main__':
+    bpm = redflagbpm.BPMService()
+    validar_solicitudes_no_vacias(bpm)
+    tipoResultado = bpm.context.input['tipoResultado']
+    custom_message(tipoResultado)
