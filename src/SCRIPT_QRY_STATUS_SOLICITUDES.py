82b9a024c7c776f48fad7bc9a619c471fa5cee1f
electric-lady <lioren.bessone.jofre@mi.unc.edu.ar>
2024-01-05 17:20:02 -0300

    modificando los cqaptions de la consulta de solicitudes

--- src/SCRIPT_QRY_STATUS_SOLICITUDES.py
+++ src/SCRIPT_QRY_STATUS_SOLICITUDES.py
@@ -5,14 +5,14 @@ import psycopg2
 import psycopg2.extras
 from redflagbpm import PgUtils
 
-from DB import _get_hg_connection
+from DB import _get_hg_connection, _get_connection
 import datetime
 from decimal import Decimal
 def get_solicitudes(bpm, conn, tipo_solicitud):
     # me conecto a la DB remota
     dblink = PgUtils.get_dblink(bpm, "SYC")
-
-    conn = _get_hg_connection(bpm)
+    #me conecto a la DB local
+    conn = _get_connection(bpm)
 
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
@@ -20,7 +20,7 @@ def get_solicitudes(bpm, conn, tipo_solicitud):
     sql = """    
             with hg as(
                   select *
-                  from dblink(%%s,
+                  from dblink(%s,
                   $$
                             with solicitudes as (
                             select 
@@ -90,7 +90,7 @@ def get_solicitudes(bpm, conn, tipo_solicitud):
                             from solicitudes
                             where 
                              fecha = current_date
-                            and lower(tipo_solicitud) = lower(''Suscripcion'')
+                            and lower(tipo_solicitud) = lower(''%s'')
                             order by 1 desc
                             limit 100
                   $$) as t(id_origen bigint, tipo_solicitud character varying, codigo_fci bigint, fci character varying, cuenta_id bigint,	
@@ -100,24 +100,30 @@ def get_solicitudes(bpm, conn, tipo_solicitud):
                 ),
                 bpm_base as (
                     select 
-                        p.business_key_ as "businessKey",
+                        p.business_key_ as "business_key",
                         p.start_time_ as "start",
                         p.end_time_ as "end",
                         p.start_user_id_ as "initiator",
+                        vacc.text_ as "cuenta_id",
+                        case when p.business_key_ like 'INSTFCISU%%' then 'suscripcion'
+                        else 'rescate'
+                        end as tipo_solicitud,
                         coalesce(arr_pend.text_, convert_from(byte_pend.BYTES_, 'UTF8'))::jsonb ||
                         coalesce(arr_conf.text_, convert_from(byte_conf.BYTES_, 'UTF8'))::jsonb as array_pend_conf
                     from act_hi_procinst p 
                         inner join act_hi_varinst vini on p.proc_inst_id_ = vini.proc_inst_id_ and vini.name_ = 'initiator'
+                        inner join act_hi_varinst vacc on p.proc_inst_id_ = vacc.proc_inst_id_ and vacc.name_ = 'cuenta'
                         inner join act_hi_varinst arr_pend on p.proc_inst_id_ = arr_pend.proc_inst_id_ and arr_pend.name_ = 'array_solicitudes_pendientes'
                         left join ACT_GE_BYTEARRAY byte_pend ON arr_pend.BYTEARRAY_ID_ = byte_pend.ID_	
                         inner join act_hi_varinst arr_conf on p.proc_inst_id_ = arr_conf.proc_inst_id_ and arr_conf.name_ = 'array_solicitudes_confirmadas'
                         left join ACT_GE_BYTEARRAY byte_conf ON arr_pend.BYTEARRAY_ID_ = byte_conf.ID_
-                    where business_key_ like 'INSTFCI%'
+                    where business_key_ like 'INSTFCI%%'
                 ),
                 bpm_explotada as (
-                    select "businessKey", "start", "end", "initiator", 
+                    select "business_key", "start", "end", "initiator", cuenta_id, tipo_solicitud,
                         sol#>>'{error}' as error,
                         sol#>>'{fondo}' as fondo,
+                        (REGEXP_MATCHES(sol#>>'{fondo}', '([0-9]+)'))[1] AS codigo_fci,
                         (sol#>>'{monto}')::decimal as monto,
                         (sol#>>'{cantidad_importe}')::decimal as cantidad_importe,
                         (sol#>>'{precio}')::decimal as precio,
@@ -126,17 +132,40 @@ def get_solicitudes(bpm, conn, tipo_solicitud):
                     from bpm_base as bpm
                     cross join lateral jsonb_array_elements(bpm.array_pend_conf) as s (sol)
                 )
-                select
-                (select 
-                    STRING_AGG(participante, ', ') AS participante_list
-                    from ds.participantes
-                    where team = p.team)::character varying as team_members, *
-                from bpm_explotada as bpm
-                full join hg on bpm.numero_solicitud = hg.id_origen
-                left join ds.participantes p on hg.propietario_tarea = p.participante
+            select
+            bpm.business_key,
+            bpm.numero_solicitud,
+            bpm.initiator,
+            bpm.cuenta_id,
+            bpm.tipo_solicitud as tipo_solicitud_bpm,
+            bpm.start,
+            bpm.end,
+            bpm.fondo,
+            bpm.codigo_fci,
+            bpm.monto,
+            bpm.moneda,
+            bpm.cantidad_importe,
+            bpm.precio,
+            hg.id_origen,
+            hg.tipo_solicitud tipo_solicitud_hg,
+            hg.propietario_tarea,
+            hg.codigo_fci,
+            hg.cuenta_id,
+            hg.estado as estado_hg,
+            hg.fecha,
+            hg.cantidad,
+            hg.cantidad_cuotapartes,
+            (select 
+            STRING_AGG(participante, ', ') AS participante_list
+            from ds.participantes
+            where team = p.team)::character varying as team_members, 
+            template
+            from bpm_explotada as bpm
+            full join hg on bpm.numero_solicitud = hg.id_origen
+            left join ds.participantes p on hg.propietario_tarea = p.participante
         """
 
-    cur.execute(sql, (dblink, ))
+    cur.execute(sql, (dblink, tipo_solicitud, ))
     qry = cur.fetchall()
 
     cur.close()
@@ -149,7 +178,7 @@ def main():
     qry = get_solicitudes(bpm=bpm, conn=conn, tipo_solicitud='Suscripcion')
 
 
-    print(qry)
+    # print(qry)
     class Encoder(json.JSONEncoder):
         def default(self, obj):
             if isinstance(obj, (datetime.date, datetime.datetime)):
