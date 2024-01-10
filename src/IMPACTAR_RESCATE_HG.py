c93107fd19dfb0adfe067358988c7877da9ac937
electric-lady <lioren.bessone.jofre@mi.unc.edu.ar>
2024-01-10 11:41:11 -0300

    aregando modificaciones a la lectura de los arrays de rescate para handlear en forma diferenciada los errores

--- src/IMPACTAR_RESCATE_HG.py
+++ src/IMPACTAR_RESCATE_HG.py
@@ -79,10 +79,12 @@ def rescatar(fecha, cuenta_id, array_solicitudes_pendientes, solicitud, array_so
     # resp_alta_ok, mje = True, None
     if not resp_alta_ok:
         solicitud["error"] = mje
+        return mje
     else:
         array_solicitudes_pendientes.remove(solicitud)
         solicitud["numero_solicitud"] = response.json()["solicitud"]['numeroSolicitud']
         array_solicitudes_confirmadas.append(solicitud)
+        return None
     logging.info('Thread %s: finishing', name)
 
 if __name__ == '__main__':
@@ -95,34 +97,59 @@ if __name__ == '__main__':
 
         token = login(url_base)
         #todo unncoment in local tests only
-        get_backtesting_redemption_data(bpm)
+        # get_backtesting_redemption_data(bpm)
 
         # el form manda la fecha en milisegundos al proceso
         # y el proceso la guarda como fecha de java
         # -> uso .getTime() para obtener los milisegundos
         fecha = formatear(bpm.context['fecha.getTime()'])
-        cuenta_id= bpm.context['cuenta']
+        cuenta_id = bpm.context['cuenta']
         array_solicitudes_pendientes = bpm.context['array_solicitudes_pendientes']
         try:
             array_solicitudes_confirmadas = bpm.context['array_solicitudes_confirmadas']
         except:
             array_solicitudes_confirmadas = []
 
-
-        thread_list = []
+        #defino la lista future_results donde almaceno la "promesa" de la respuesta de todos
+        # los threads
+        future_results = []
         i = 1
         with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
             i=1
             for solicitud in array_solicitudes_pendientes:
                 logging.info('Main    :before running thread')
-                executor.submit(rescatar, fecha=fecha, cuenta_id=cuenta_id, array_solicitudes_pendientes=array_solicitudes_pendientes, solicitud=solicitud, array_solicitudes_confirmadas=array_solicitudes_confirmadas, thread_name=i)
+                futuro = executor.submit(rescatar, fecha=fecha, cuenta_id=cuenta_id, array_solicitudes_pendientes=array_solicitudes_pendientes, solicitud=solicitud, array_solicitudes_confirmadas=array_solicitudes_confirmadas, thread_name=i)
+                future_results.append(futuro)
                 i+=1
+
+        final_results = [result.result() for result in future_results]
 
         if len(array_solicitudes_pendientes) == 0:
             # todo coment in local tests only
             bpm.execution.setVariable('accion', "continuar")
         else:
+            pattern = '(Ya existe una solicitud pendiente con estos datos. Por favor actualice la solicitud previa.)'
+            # itero el resultado de todos los threads
+            for res in final_results:
+                # comparo con el error de solicitud repetida
+                try:
+                    err_solicitud_duplicada = re.search(pattern, res).group(1)
+                except AttributeError:
+                    err_solicitud_duplicada = None
+                # si no hay error o es un error por otro motivo -> error x duplicado es falso
+                if err_solicitud_duplicada is None:
+                    error_x_duplicado = False
+                # si el error es por solicitud repetida-> error x duplicado es verdadero (y sobreescribe le valor anterior)
+                else:
+                    error_x_duplicado = True
+
+        # si hay algún error por solicitud repetida -> accion: reintentar
+        if error_x_duplicado:
+            bpm.execution.setVariable('accion', 'reintentar')
+        # si hay error pero no de duplicados ->accion: corregir
+        else:
             bpm.execution.setVariable('accion', 'corregir')
+
         bpm.execution.setVariable('array_solicitud_pendiente', array_solicitudes_pendientes)
         bpm.execution.setVariable('array_solicitud_confirmada', array_solicitudes_confirmadas)
     except:
