e8c8b99f8236aeb7ebabddd967f9f2ca81244c7e
electric-lady <lioren.bessone.jofre@mi.unc.edu.ar>
2024-01-12 11:55:45 -0300

    agregando mensaje custom

---
+++ src/CUSTOM_MESSAGE.py
@@ -0,0 +1,12 @@
+#!python3
+import redflagbpm
+def custom_message(tipoResultado):
+    if tipoResultado == "ok":
+        message = "Solicitud procesada"
+
+        bpm.context.input['message'] = message
+
+if __name__ == '__main__':
+    bpm = redflagbpm.BPMService()
+    tipoResultado = bpm.context.input['tipoResultado']
+    custom_message(tipoResultado)
