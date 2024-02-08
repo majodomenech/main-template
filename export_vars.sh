aff8eff04c77b20e991c6819c0881aaf85cd06f0
SyC Inversiones <admin@sycinversiones.com.ar>
2024-02-08 14:17:16 -0300

    revert

--- export_vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export tipo_solicitud='rescate'
-export fechaConsultaDesde=1702177200000
-export fechaConsultaHasta=1704855600000
-export cuenta_id=5004543
-export fondo_id=14209
-export tipo_solicitud_bpm='suscripcion'
