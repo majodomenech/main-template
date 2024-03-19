b8266023ca4c74a1a128df120b3f71917a9367f5
Javier Luna <jluna@sycinversiones.com>
2024-03-19 15:01:33 -0300

    query menu script corregido

--- src/QUERY_MENU_SCRIPT.py
+++ src/QUERY_MENU_SCRIPT.py
@@ -1,16 +1,19 @@
 #!python3
 import json
-import re
-import urllib
-from datetime import date, timedelta
-
 import pandas as pd
 import redflagbpm
-from decimal import Decimal
-from UTILS import call_json_endpoint, _get_connection, call_endpoint_gara, get_comit_responsable_nd_team, \
-    get_esp_representacion, get_operador_hg
+from redflagbpm import PgUtils
 import psycopg2
 import psycopg2.extras
+
+
+def _get_connection():
+    bpm = redflagbpm.BPMService()
+    # Manual connection, no config file
+    conn = PgUtils.get_connection(bpm, 'FLW')
+    conn.autocommit = True
+    return conn
+
 
 def qry_todos(conn):
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
@@ -26,7 +29,8 @@ def qry_todos(conn):
     conn.close()
     return pd.DataFrame(qry)
 
-def qry_usuario(conn):
+
+def qry_usuario(conn, nombre):
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
     sql = """ with usuario as (
@@ -51,21 +55,26 @@ from usuario
 
         """
 
-    cur.execute(sql,(nombre,))
+    cur.execute(sql, (nombre,))
     qry = cur.fetchall()
     cur.close()
     conn.close()
     return pd.DataFrame(qry)
 
+
 def main():
     bpm = redflagbpm.BPMService()
     tipo = bpm.context['tipo']
+    try:
+        nombre = bpm.context['usuario']
+    except:
+        nombre = bpm.context['userId']
 
     if tipo == 'Usuario':
-        conn = _get_connection(bpm)
-        df = qry_usuario(conn)
+        conn = _get_connection()
+        df = qry_usuario(conn, nombre)
     else:
-        conn = _get_connection(bpm)
+        conn = _get_connection()
         df = qry_todos(conn)
 
     data = df.to_dict(orient='records')
@@ -78,6 +87,7 @@ def main():
     _responseHeaders["Content-Type"] = "application/json"
     _responseHeaders["Content-Encoding"] = "UTF-8"
     _responseHeaders["resource"] = "/tmp/qry_menu.json"
+
 
 if __name__ == '__main__':
     main()
