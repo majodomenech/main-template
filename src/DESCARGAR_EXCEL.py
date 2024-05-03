da400844f25850b8de9ccb8aed7acf38e7b3befc
Javier Luna <jluna@sycinversiones.com>
2024-05-03 13:10:49 -0300

    cambiado el config

--- src/DESCARGAR_EXCEL.py
+++ src/DESCARGAR_EXCEL.py
@@ -22,30 +22,12 @@ def _get_connection():
 
 
 def consultar_pedidos_dia():
-    dia = bpm.context['dia']
 
     sql_pos = """
-           with
-pedidos as (select CONCAT(last_, ' ', first_) as nombre, """ + dia + """ 
-            from menu.menu m
-            join act_id_user a
-            on m.nombre = a.id_
-			where """ + dia + """!= 'AUSENTE' and """ + dia + """ !=''
-			order by 1 asc 
-			),
-
-ausentes as (select CONCAT(last_, ' ', first_) as nombre, """ + dia + """ 
-            from menu.menu m
-            join act_id_user a
-            on m.nombre = a.id_
-			where """ + dia + """= 'AUSENTE' or """ + dia + """ =''
-			order by 1 asc 
-			)
-select *
-from pedidos
-union all
-select *
-from ausentes
+           select *
+from colocadoras_fci.cauciones
+where monto !=0 or operado !=0
+order by monto desc
 
             """
     conn = _get_connection()
@@ -56,28 +38,8 @@ from ausentes
     return pd.DataFrame(data)
 
 
-def contar_pedidos():
-    dia = bpm.context['dia']
 
-    sql_pos = """   with pedidos as (select CONCAT(first_, ' ', last_) as nombre,""" + dia + """
-                    from menu.menu m
-                    join act_id_user a
-                    on m.nombre = a.id_
-                    where """ + dia + """ != 'AUSENTE' and """ + dia + """!=''
-                    )
-                    select count(*) as cantidad, """ + dia + """ as menu
-                    from pedidos
-                    group by 2
-                """
-    conn = _get_connection()
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute(sql_pos)
-    data = cur.fetchall()
-    cur.close()
-    return pd.DataFrame(data)
-
-
-def generar_excel(df1, df2):
+def generar_excel():
     # Create a new Excel workbook
     wb = Workbook()
 
@@ -86,19 +48,17 @@ def generar_excel(df1, df2):
     wb.remove(default_sheet)
 
     # Create a new sheet for each DataFrame
-    ws1 = wb.create_sheet(title='Pedidos')
-    ws2 = wb.create_sheet(title='Cantidades')
-    return wb, ws1, ws2
+    ws1 = wb.create_sheet(title='Cauciones')
+    return wb, ws1
 
 
 def write_sheet(sheet, dataframe):
     # Copiar el DataFrame a la hoja de Excel
-    dia = bpm.context['dia']
     # Obtener el día actual
     fecha_actual = datetime.datetime.now()
     fecha = fecha_actual.strftime("%d/%m")
     hora = datetime.datetime.now().strftime("%H:%M")
-    texto_fila = f"{dia} {fecha} {hora}"
+    texto_fila = f"{fecha} {hora}"
 
     # Obtener la primera celda de la nueva fila
     cell = sheet.cell(row=1, column=1)
@@ -149,16 +109,14 @@ def write_sheet(sheet, dataframe):
 
 def main():
     df_pedidos = consultar_pedidos_dia()
-    df_pedidos.columns = ['Nombre', 'Menú']
+    #df_pedidos.columns = ['Nombre', 'Menú']
 
-    df_cantidades = contar_pedidos()
-    df_cantidades.columns = ['Cantidad', 'Menú']
-    wb, ws1, ws2 = generar_excel(df_pedidos, df_cantidades)
+    wb, ws1 = generar_excel()
     write_sheet(ws1, df_pedidos)
-    write_sheet(ws2, df_cantidades)
+
     wb.save('/tmp/Pedidos.xlsx')
 
 
 main()
-bpm.context.setJsonValue("_responseHeaders", "content-type", "application/vnd.ms-excel")
-bpm.context.setJsonValue("_responseHeaders", "resource", "/tmp/Pedidos.xlsx")
+#bpm.context.setJsonValue("_responseHeaders", "content-type", "application/vnd.ms-excel")
+#bpm.context.setJsonValue("_responseHeaders", "resource", "/tmp/Pedidos.xlsx")
